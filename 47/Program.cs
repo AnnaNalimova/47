7853156b347376c54e387d446fbf7c0c1666be86
Anna Nalimova <avnalimova@mail.ru>
2023-03-10 09:14:14 +0300

    47

---
+++ 47/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двухмерный массив размером m*n ,
+// заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n, int min,int max)
+{
+    double [,] array = new double[m,n];
+    Random rand = new Random();
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+      for(int j=0;j<array.GetLength(1);j++)
+      {
+         array[i,j]=  Math.Round(rand.Next(min,max) + rand.NextDouble(),3);
+      }
+    }
+return array;
+}
+void PrintArray(double[,] mass)
+{
+    for( int i=0;i<mass.GetLength(0);i++)
+    {
+        for( int j=0;j<mass.GetLength(1);j++)
+        {
+            Console.Write($"{mass[i, j]}");
+        }
+    Console.WriteLine();
+    }
+}
+double [,] massive = GetArray(3,4,-2,3);
+PrintArray(massive);
